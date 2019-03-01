ad1eca69ebd64d75e87b3a9e9b468f63e691713d
Alex <alex2409@t-online.de>
2019-03-01 22:13:44 +0100

    .

--- start.py
+++ start.py
@@ -1,6 +1,23 @@
 from microWebSrv import MicroWebSrv
 
 import json
+import machine
+
+
+red = machine.PWM(machine.Pin(4))
+green = machine.PWM(machine.Pin(2))
+blue = machine.PWM(machine.Pin(18))
+red.freq(122)
+green.freq(122)
+blue.freq(122)
+
+
+def show_color(hexcolor):
+    h= hexcolor.lstrip('#')
+    color_rgb = tuple(int(h[i:i+2], 16) for i in (0, 2 ,4))
+    red.duty(color_rgb[0])
+    green.duty(color_rgb[1])
+    blue.duty(color_rgb[2])
 
 
 def _httpHandlerLEDPost(httpClient, httpResponse):
@@ -12,3 +29,4 @@ def _httpHandlerLEDPost(httpClient, httpResponse):
 routeHandlers = [ ( "/led-try", "POST",  _httpHandlerLEDPost ) ]
 srv = MicroWebSrv(routeHandlers=routeHandlers, webPath='/www/')
 srv.Start(threaded=False)
+show_color('#ffffff')
