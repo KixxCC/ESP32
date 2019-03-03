57c02947597add294180ef807c928c9be5ba52be
Alex <alex2409@t-online.de>
2019-03-03 18:08:19 +0100

    .

--- start.py
+++ start.py
@@ -1,18 +1,21 @@
+
+
 from microWebSrv import MicroWebSrv
 
 import json
 import machine
 
 
-red = machine.PWM(machine.Pin(4))
-green = machine.PWM(machine.Pin(2))
-blue = machine.PWM(machine.Pin(18))
-red.freq(122)
-green.freq(122)
-blue.freq(122)
+
 
 
 def show_color(hexcolor):
+    red = machine.PWM(machine.Pin(4))
+    green = machine.PWM(machine.Pin(2))
+    blue = machine.PWM(machine.Pin(18))
+    red.freq(122)
+    green.freq(122)
+    blue.freq(122)
     h= hexcolor.lstrip('#')
     color_rgb = tuple(int(h[i:i+2], 16) for i in (0, 2 ,4))
     red.duty(color_rgb[0])
@@ -21,8 +24,23 @@ def show_color(hexcolor):
 
 
 def _httpHandlerAddColorPost(httpClient, httpResponse):
+    with open('/www/colors.json') as colors_json:
+        colors_raw = json.load(colors_json)
+    all_colors_json = dict(**colors_raw['defaults'], **colors_raw['user_set'])
     content = httpClient.ReadRequestContent()
-    color = json.loads(content)
+    content_json = json.loads(content)
+    name,color = content_json['name'],content_json['color']
+    if name in all_colors_json.keys():
+        pass
+        #send name already exists error
+    if color in all_colors_json.values():
+        pass
+        #send color already exists with name error
+    else:
+        colors_raw['user_set'].update({name:color})
+        with open('/www/colors.json','w') as colors_json:
+            colors_json.write(json.dumps(colors_raw,indent=4))
+        #send succesfull submit response
 
 def _httpHandlerLEDTryPost(httpClient, httpResponse):
     print("got somenthing")
